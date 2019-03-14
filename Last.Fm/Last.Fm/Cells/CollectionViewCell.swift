80d2c9deeb922d9739e5d192a649d7c9e75f804b
cveeras <veera.chitte@wipro.com>
2019-03-14 17:29:35 +0530

    created collection view with cell and expoected required labels in storyboard by adding files

--- Last.Fm/Last.Fm/Cells/CollectionViewCell.swift
+++ Last.Fm/Last.Fm/Cells/CollectionViewCell.swift
@@ -10,4 +10,9 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet var albumImageView: UIImageView!
+    @IBOutlet var transImageView: UIImageView!
+    @IBOutlet var albumName: UILabel!
+    @IBOutlet var artistName: UILabel!
+    
 }
--- Last.Fm/Last.Fm/Controllers/ViewController.swift
+++ Last.Fm/Last.Fm/Controllers/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var collectionSearchBar: UISearchBar!
+    @IBOutlet var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
