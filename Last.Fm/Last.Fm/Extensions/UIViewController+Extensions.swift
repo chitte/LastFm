7da80bbf48d373f76bfc3e46e6a4c5c931222feb
VeeraSu <chittesubhash3@gmail.com>
2019-04-05 13:20:50 +0530

    extensions code clean up

--- Last.Fm/Last.Fm/Extensions/UIViewController+Extensions.swift
+++ Last.Fm/Last.Fm/Extensions/UIViewController+Extensions.swift
@@ -17,4 +17,28 @@ extension UIViewController {
             self.present(alert, animated: true, completion: nil)
         }
     }
+
+    public func showAnimation(rootVC: UIViewController, shouldStartAnimation: Bool) {
+        let animationView = UIView()
+        let animationSpinner = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.whiteLarge)
+
+        if shouldStartAnimation {
+            animationView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+            animationView.backgroundColor = UIColor.black.withAlphaComponent(0.1)
+            rootVC.view.addSubview(animationView)
+            rootVC.view.bringSubviewToFront(animationView)
+
+            animationSpinner.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+            animationSpinner.center = animationView.center
+            animationView.tag = 55555
+            animationSpinner.startAnimating()
+            animationView.addSubview(animationSpinner)
+        } else {
+            if let viewWithTag = rootVC.view.viewWithTag(55555) {
+                animationSpinner.stopAnimating()
+                viewWithTag.removeFromSuperview()
+            }
+        }
+    }
 }
+
